75627f84e757121a539754496012bc9ef1ba22c7
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-08 04:52:16 +0530

    kickstart modular

---
+++ init.lua
@@ -0,0 +1,37 @@
+-- [[ Setup initial configuration ]]
+
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Set to true if you have a Nerd Font installed and selected in the terminal
+vim.g.have_nerd_font = false
+
+vim.g.mapleader = " "
+
+require("core")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+-- Add lazy to the `runtimepath`, this allows us to `require` it.
+---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- Set up lazy, and load my `lua/plugins/` folder
+require("lazy").setup({ import = "plugins" }, {
+  change_detection = {
+    notify = false,
+  },
+})
