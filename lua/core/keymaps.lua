f005a15e470aaecfb05cbbf18868d7439f9b8689
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-11 23:50:28 +0530

    keymap to toggle relative line numbers

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -19,6 +19,14 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
+-- keymap to toggle relative line number
+vim.keymap.set('n', '<leader>n', function()
+  if vim.o.relativenumber == true then
+    vim.o.relativenumber = false
+  else
+    vim.o.relativenumber = true
+  end
+end, { desc = 'Toggle relative line numbers' })
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -33,4 +41,3 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 -- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 -- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
-
