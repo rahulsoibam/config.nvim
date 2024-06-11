b2728b296ded267f52d2100e01d104b1a763a013
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-11 23:51:11 +0530

    set relative line number as default

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -3,11 +3,9 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
-vim.opt.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+-- line numbers
+vim.opt.relativenumber = true -- show relative line numbers
+vim.opt.number = true -- shows absolute line number on cursor line (when relative number is on)
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
