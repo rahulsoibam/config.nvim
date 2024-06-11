7c9f22a48a22fac46a733c90da2384368ffaf181
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-12 01:28:55 +0530

    add more options; added dressing.nvim

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -21,27 +21,54 @@ vim.opt.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.opt.breakindent = true
 
+-- tabs & indentation
 vim.opt.smartindent = true
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.autoindent = true -- copy indent from current line when starting new one
+vim.opt.shiftwidth = 4 -- 4 spaces for indent width
+vim.opt.expandtab = true -- expand tab to spaces
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4 -- 4 spaces for tabs (prettier default)
 
+-- line wrapping
+vim.opt.wrap = true -- disable line wrapping
+
+-- turn off swapfile
+-- vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
 -- Save undo history
 vim.opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
 
--- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+-- [[ search settings ]]
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.incsearch = true
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+-- TIP :%s/\Capple/orange/g
+-- The \C forces the search to be case-sensitive.
+-- This is useful if you have set case-insensitive searching as your default but want to override it for a specific search and replace.
+
+-- [[ appearance ]]
+-- turn on termguicolors for colorschemes to work
+-- (have to use iterm2 or any other true color terminal)
+vim.opt.termguicolors = true
+vim.opt.background = 'dark' -- colorschemes that can be light or dark will be made dark
+vim.opt.signcolumn = 'yes' -- show sign column so that text doesn't shift
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- backspace
+vim.opt.backspace = 'indent,eol,start' -- allow backspace on indent, end of line or insert mode start position
 
 -- Decrease update time
 vim.opt.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.opt.timeoutlen = 200
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
@@ -53,13 +80,7 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Show which line your cursor is on
-vim.opt.cursorline = true
-
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 20
 
-vim.opt.expandtab = true
+vim.opt.colorcolumn = '120'
---
+++ lua/plugins/dressing.lua
@@ -0,0 +1,5 @@
+return {
+  --[[ Neovim plugin to improve the default vim.ui interfaces ]]
+  'stevearc/dressing.nvim',
+  opts = {},
+}
