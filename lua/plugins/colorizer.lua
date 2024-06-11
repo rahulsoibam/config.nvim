fb0b703afab553f9674286136fb52b95534dac6b
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-12 02:11:53 +0530

    added colorizer
    spent a lot of time here because the google search for nvim-colorizer
    returns norcalli/nvim-colorizer.lua, which is very buggy.
    A fork of the same, NvChad/nvim-colorizer.lua works as expected.

---
+++ lua/plugins/colorizer.lua
@@ -0,0 +1,16 @@
+return {
+  -- NORCALLI DOES NOT WORK PROPERLY. WASTED A LOT OF TIME ON THIS. F**K.
+  -- 'norcalli/nvim-colorizer.lua',
+  'NvChad/nvim-colorizer.lua',
+  event = { 'BufReadPre', 'BufNewFile' },
+  config = function()
+    require('colorizer').setup {
+      filetypes = {
+        css = { rgb_fn = true },
+        'javascript',
+        html = { mode = 'foreground' },
+      },
+      user_default_options = { mode = 'background' },
+    }
+  end,
+}
