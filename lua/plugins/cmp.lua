4f8244ab0fab092e6202f6d03ca281cd79d417aa
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-12 03:49:50 +0530

    updated cmp keymap for scroll_docs

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -62,9 +62,9 @@ return { -- Autocompletion
         -- Select the [p]revious item
         ['<C-p>'] = cmp.mapping.select_prev_item(),
 
-        -- Scroll the documentation window [b]ack / [f]orward
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- Scroll the documentation window [u]p / [d]own
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
 
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
