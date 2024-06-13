fdabf164ad620df2c711bd959ca390ff5d87d853
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-13 09:10:58 +0530

    make indent-blankline look better

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,9 +1,24 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+  -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  config = function()
+    require('ibl').setup {
+      indent = {
+        char = '▏', -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+      },
+      scope = {
+        show_start = false,
+        show_end = false,
+      },
+    }
+
+    -- disable indentation on the first level
+    local hooks = require 'ibl.hooks'
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+  end,
+  opts = {},
 }
