4d34b61b2df4601867c317266636e8a88c213453
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-13 09:02:32 +0530

    added diagnostics symbols

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -84,3 +84,10 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.scrolloff = 20
 
 vim.opt.colorcolumn = '120'
+
+-- [[ set diagnostic symbols ]]
+local symbols = { Error = '󰅙', Info = '󰋼', Hint = '󰌵', Warn = '' }
+for name, icon in pairs(symbols) do
+  local hl = 'DiagnosticSign' .. name
+  vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
