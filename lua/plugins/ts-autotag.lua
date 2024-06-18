03451cddb32433056015c4f4f366fdbdb77a603c
Rahul Soibam <rahulsoibam@gmail.com>
2024-06-18 16:52:37 +0530

    enable close tags

--- lua/plugins/ts-autotag.lua
+++ lua/plugins/ts-autotag.lua
@@ -5,7 +5,7 @@ return {
     require('nvim-ts-autotag').setup({
       opts = {
         -- Defaults
-        enable_close = false, -- Auto close tags
+        enable_close = true, -- Auto close tags
         enable_rename = true, -- Auto rename pairs of tags
         enable_close_on_slash = true, -- Auto close on trailing </
       },
