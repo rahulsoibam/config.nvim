66620eed07d889395fecf8008eccf2dfad32199b
Rahul Soibam <rahulsoibam@gmail.com>
2024-10-04 12:59:46 +0530

    add some comments

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -12,12 +12,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- save view folds when leaving buffer
 vim.api.nvim_create_autocmd({ 'BufWinLeave' }, {
   pattern = { '*.*' },
   group = vim.api.nvim_create_augroup('remember_folds', { clear = false }),
   desc = 'save view (folds), when closing file',
   command = 'mkview',
 })
+
+-- load view folds when opening file
 vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
   pattern = { '*.*' },
   group = vim.api.nvim_create_augroup('remember_folds', { clear = false }),
